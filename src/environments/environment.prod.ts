3478c16c25fd079a083562957562a51bc0dca0d5
Malcom-Chakoma <malcom@aficanunicorn.co.zw>
2021-05-12 09:09:17 +0200

    pointing to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-   api_url:'http://localhost:3000/api/v1/',
+  //  api_url:'http://localhost:3000/api/v1/',
   // api_url: 'https://mydoced.com/app/api/',
-  // api_url:' https://doced-backend.herokuapp.com/api/',
+  api_url:' https://doced-backend.herokuapp.com/api/v1/',
 };
